60f3a862e75a95ee2d86671553049d0ff0f93134
Annabel Shaw <142178131+annabelshaw@users.noreply.github.com>
2023-08-13 14:53:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Display phrase on the webpage
+var webPhraseElement = document.getElementById('webPhrase');
+webPhraseElement.textContent = 'My first JavaScript webpage.';
+
+// Display phrase in the console
+var consolePhrase = 'This phrase is displayed in the console.';
+console.log(consolePhrase);
